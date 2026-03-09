7a392150f564c6be4e65a8bd2336984af4d03258
Omar E. Khalifa <omar.khalifa.dev@gmail>
2026-03-09 21:26:38 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Placeholder(),
+    );
+  }
+}
