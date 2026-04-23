d650b993242b73be7b29417990ec0030e3a9e6c3
Omar E. Khalifa <omar.khalifa.dev@gmail>
2026-04-23 09:56:08 +0200

    finished EditNoteView UI

--- lib/views/edit_note_view.dart
+++ lib/views/edit_note_view.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/widgets/custom_button.dart';
+import 'package:notes_app/widgets/custom_text_field.dart';
 
 class EditNoteView extends StatelessWidget {
-  const EditNoteView({super.key});
+  const EditNoteView({super.key, required this.note});
+
+  final NoteModel note;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(
           title: 'Edit Note', icon: Icons.check, titleSize: 20, onTap: () {}),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 15),
+            CustomTextField(
+                hintText: note.title,
+                borderColor: Colors.white,
+                hintColor: kPrimaryColor,
+                cursorColor: kPrimaryColor,
+                textInputAction: TextInputAction.next),
+            CustomTextField(
+              hintText: note.description,
+              borderColor: Colors.white,
+              hintColor: kPrimaryColor,
+              cursorColor: kPrimaryColor,
+              height: 200,
+              textInputType: TextInputType.multiline,
+              textInputAction: TextInputAction.newline,
+            ),
+            SizedBox(height: 30),
+            CustomButton(),
+            SizedBox(height: 15),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/views/read_note_view.dart
+++ lib/views/read_note_view.dart
@@ -20,7 +20,7 @@ class ReadNoteView extends StatelessWidget {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => EditNoteView(),
+              builder: (context) => EditNoteView(note: note,),
             ),
           );
         },
